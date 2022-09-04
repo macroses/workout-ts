ffe06ddd454d173f1738960140ef1f1df260f703
konstantin <kostyabashkov@gmail.com>
2022-09-04 10:43:29 +0300

    small fix

--- src/components/workoutForm/PickedExercises.vue
+++ src/components/workoutForm/PickedExercises.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import Icon from "@/components/ui/Icon.vue";
 import { useStore } from "@/stores/store";
+import type {Exercise} from "@/types/interface";
 
 const store = useStore();
+const x = (item: Exercise) => {
+  console.log(item)
+}
 </script>
 
 <template>
@@ -11,10 +15,12 @@ const store = useStore();
       v-for="item in store.pickedExercises"
       :key="item.id"
       class="picked-exercises__item"
-      @click="log(item)"
+      @click="x(item)"
     >
-      {{ item.name }}
-      <Icon width="12px" icon-name="angle-down"/>
+      <div class="picked-exercise__title">
+        {{ item.name }}
+        <Icon width="12px" icon-name="angle-down"/>
+      </div>
     </li>
   </ul>
 </template>
